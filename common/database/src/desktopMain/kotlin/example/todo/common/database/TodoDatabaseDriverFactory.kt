018a37cb5af8fd68a7ecff45f614909847b6edba
Ilya Gulya <ilyagulya@gmail.com>
2023-12-24 21:48:30 +0600

    desktop: fix db not being initialized because of async schema creation not being awaited

--- common/database/src/desktopMain/kotlin/example/todo/common/database/TodoDatabaseDriverFactory.kt
+++ common/database/src/desktopMain/kotlin/example/todo/common/database/TodoDatabaseDriverFactory.kt
@@ -2,14 +2,16 @@ package example.todo.common.database
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import com.badoo.reaktive.coroutinesinterop.singleFromCoroutine
+import com.badoo.reaktive.single.Single
 import example.todo.database.TodoDatabase
 import java.io.File
 
 @Suppress("FunctionName") // FactoryFunction
-fun TodoDatabaseDriver(): SqlDriver {
+fun TodoDatabaseDriver(): Single<SqlDriver> = singleFromCoroutine {
     val databasePath = File(System.getProperty("java.io.tmpdir"), "ComposeTodoDatabase.db")
-    val driver = JdbcSqliteDriver(url = "jdbc:sqlite:${databasePath.absolutePath}")
-    TodoDatabase.Schema.create(driver)
 
-    return driver
+    JdbcSqliteDriver(url = "jdbc:sqlite:${databasePath.absolutePath}").also { driver ->
+        TodoDatabase.Schema.create(driver).await()
+    }
 }
